fedf3617e32ffb1e2693709cc7b6efde3e056713
Glitch (muscled-dev) <none>
2022-04-12 20:18:23 +0000

    Initial push from Glitch

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# muscled
-MUlti-SCreen Layout EDitor
+# Multi-Screen Layout Editor
+
+The MUlti-SCreen Layout EDitor provides a 3D workspace to layout and assess the design of your multi-screen envrionment. 
+
+Try it [here](https://muscled.glitch.me/)!
+
+Rig model from [3D Warehouse](https://3dwarehouse.sketchup.com/model/u2c552875-6306-43c1-a89b-e6d9c7a554c9/Sim-Racing-Rig).  
+Background images from [triplemonitorbackgrounds.com](https://www.triplemonitorbackgrounds.com/).  
---
+++ screen.js
@@ -0,0 +1,30 @@
+import * as THREE from "three";
+
+export default class Screen extends THREE.Mesh {
+  constructor(background) {
+    super();
+
+    this.geometry = new THREE.BoxGeometry(32, 18, 0.5);
+
+    const plasticMaterial = new THREE.MeshBasicMaterial({
+      color: 0x222222,
+    });
+    const loader = new THREE.TextureLoader();
+
+    this.materials = [
+      plasticMaterial,
+      plasticMaterial,
+      plasticMaterial,
+      plasticMaterial,
+      new THREE.MeshBasicMaterial({ map: loader.load(background) }),
+      plasticMaterial,
+    ];
+
+    this.defaultMaterial = this.materials;
+  }
+
+  setSize(size) {
+    this.geometry.dispose();
+    this.geometry = new THREE.BoxGeometry(size.width, size.height, 0.5);
+  }
+}
