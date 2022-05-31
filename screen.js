9b57bf9b09151cac95a39eb65801ddafa0ea64b3
Glitch (muscled-dev) <none>
2022-05-31 20:48:08 +0000

    Add single screen option and initial texture aspect correction solution

--- screen.js
+++ screen.js
@@ -1,19 +1,29 @@
 import * as THREE from "three";
 
 export default class Screen extends THREE.Mesh {
-  constructor(background, offset) {
+  
+  static OFFSETS = {
+    left: 0.66,
+    middle: 0.33,
+    right: 0    
+  };
+
+  constructor(xAspect, yAspect, background, placement) {
     super();
     
     this.bezel = 0.5;
     
-    this.geometry = new THREE.BoxGeometry(32, 18, 0.5);
+    this.geometry = new THREE.BoxGeometry(xAspect, yAspect, 0.5);
     this.plasticMaterial = new THREE.MeshBasicMaterial({
       color: 0x222222,
     });
     
     this.defaultMaterial = this.material = this.plasticMaterial;
     
-    const geometry = new THREE.PlaneGeometry(32, 18);
+    this.width = xAspect;
+    this.height = yAspect;
+    
+    const geometry = new THREE.PlaneGeometry(this.width, this.height);
     const material = new THREE.MeshBasicMaterial({color: 0x0000ff, side: THREE.DoubleSide});
     
     this.display = new THREE.Mesh(geometry, material);
@@ -22,31 +32,50 @@ export default class Screen extends THREE.Mesh {
     this.display.position.z = this.position.z + 0.3;
     this.add(this.display);
     
-    this.loader = new THREE.TextureLoader();
+    this.loader = new THREE.TextureLoader();    
+    
     if(background)
       this.setImagePath(background);
     else 
       this.setImagePath();
-    if(offset || offset === 0)
-      this.setImageOffset(offset);
+    
+    this.placement = placement;
+    this.offset = Screen.OFFSETS[placement];
+    console.log(this.offset)
+    if(this.offset || this.offset === 0)
+      this.setImageOffset(this.offset);
   }
   
   setImageOffset(offset) {
     let texture  = null;
     if(this.display.material)
       texture = this.display.material.map;
+    
     if(texture) {
-      texture.repeat.x = 0.333;
+      texture.repeat.x = 1 / 3;
       texture.offset.x = offset;
     }
   }
 
   setSize(size) {
+    this.width = size.x;
+    this.height = size.y;
     this.size = size;
     this.geometry.dispose();
     this.geometry = new THREE.BoxGeometry(size.width, size.height, 0.5);
     this.display.geometry.dispose();
     this.display.geometry = new THREE.PlaneGeometry(size.width - this.bezel, size.height - this.bezel);
+    const tex = this.display.material.map;
+    if(tex.image) {
+      
+      // const x = (tex.image.width / tex.image.height) * (this.height / this.width) / 3;
+      // tex.repeat.y = x;
+      // tex.offset.y = (1 - x) / 2;
+      
+      // const x = (tex.image.width / tex.image.height) * (this.height / this.width);
+      // tex.repeat.x = 1 / x;
+      // tex.offset.x = (this.offset / x) * 3 + 1/6;
+    }
   }
   
   setBezel(size) {
@@ -56,9 +85,9 @@ export default class Screen extends THREE.Mesh {
   
   setImagePath(path) {
     let frontMaterial = this.plasticMaterial;
-    if(path)
+    if(path) {      
       frontMaterial = new THREE.MeshBasicMaterial({ map: this.loader.load(path) });
-    
+    }    
     this.display.defaultMaterial = this.display.material = frontMaterial;
   }
 }
